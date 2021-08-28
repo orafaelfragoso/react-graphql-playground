d259ccde264775f53a1795c123e13ad61caac193
Rafael Fragoso <rafael.fragoso@bolttech.io>
2021-08-28 12:30:09 -0300

    chore: add eslint and prettier configuration

---
+++ prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+}
